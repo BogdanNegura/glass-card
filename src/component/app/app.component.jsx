3ef7853e889d2b634bd80145978674278fb9faa2
Bogdan Negura <id.bogdann@yahoo.com>
2021-01-22 20:32:42 +0000

    initial cleanup

---
+++ src/component/app/app.component.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const App = () => {
+    return (
+        <div className="app">
+            <h1>Glass Effect</h1>
+        </div>
+    )
+}
+
+export { App }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { App } from './component/app';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const app = <App />
+const here = document.getElementById('react-will-render-here')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(app, here)
